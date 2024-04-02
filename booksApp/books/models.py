b33235f513eeb0b5b27acdd9fae0be8ddf4b44f3
mohamed mostafa <muhammed.moustafa.ahmed@gmail.com>
2024-04-02 21:17:02 +0200

    using model and connect to admin dashboard

--- booksApp/books/models.py
+++ booksApp/books/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    num_of_pages = models.IntegerField(null=True ,blank=True)
+    author = models.CharField(max_length=100 , null=True , blank=True)
+    price = models.IntegerField(null=True , blank=True)
+    image = models.CharField(max_length=200 , null=True , blank=True)
+    created_at = models.DateTimeField(auto_now_add=True , null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+
+
+
+
+
